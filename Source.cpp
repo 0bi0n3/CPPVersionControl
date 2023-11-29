3359a38b3b1728af5a862d9b51965c57ba39b9f8
Oberon <oberon@one-audio.co.uk>
2023-11-29 10:47:18 +0000

    Added goodbye function.

--- Source.cpp
+++ Source.cpp
@@ -6,10 +6,18 @@ void greet()
 	std::cout << "Welcome to the Feature branch Adventurers!" << std::endl;
 }
 
+void goodbye()
+{
+	std::cout << "Fare Thy Well!" << std::endl;
+}
+
 int main()
 {
 	// Print some alternative text.
 	greet();
+
+	// Print leaving message.
+	goodbye();
 
 	//std::cin.get();
 }
