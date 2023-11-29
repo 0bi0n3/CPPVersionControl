a9004a1f73a96e8f25c99731b6849d75346b68e6
Oberon <oberon@one-audio.co.uk>
2023-11-29 10:48:15 +0000

    Entered ending message.

--- Source.cpp
+++ Source.cpp
@@ -6,10 +6,17 @@ void greet()
 	std::cout << "Welcome to the Feature branch Adventurers!" << std::endl;
 }
 
+void goodbye()
+{
+	std::cout << "Your adventure has come to an end!" << std::endl;
+}
+
 int main()
 {
 	// Print some alternative text.
 	greet();
+	// Print ending message.
+	goodbye();
 
 	//std::cin.get();
 }
