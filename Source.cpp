920dfacfd97a9c42c1b61ef53b3a740d970602cf
Oberon <oberon@one-audio.co.uk>
2023-11-29 10:36:24 +0000

    Added greet function.

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <string>
 
+void greet()
+{
+	std::cout << "Welcome to the Feature branch Adventurers!" << std::endl;
+}
+
 int main()
 {
 	// Print some alternative text.
-	std::cout << "Welcome to the Feature branch Adventurers!" << std::endl;
+	greet();
 
 	//std::cin.get();
 }
